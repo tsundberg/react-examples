d09e7094142003db2c40858603ed78baca70892e
Thomas Sundberg <thomas@thinkcode.se>
2020-09-04 19:35:20 +0200

    Add a calculate button and listen for clicks on it

--- moving-state-around/src/App.tsx
+++ moving-state-around/src/App.tsx
@@ -2,16 +2,27 @@ import React from 'react';
 import './App.css';
 import CalculatorStateInLeaf from "./state-in-leaf/Calculator";
 
-function App() {
-    return (
-        <div>
-            <h2>State in leaf</h2>
-            <CalculatorStateInLeaf/>
+class App extends React.Component {
 
-            <h2>State in parent</h2>
-            <CalculatorStateInLeaf/>
-        </div>
-    );
+    private static calculate() {
+        console.log("Calculate")
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <h2>State in leaf</h2>
+                    <CalculatorStateInLeaf/>
+
+                    <h2>State in parent</h2>
+                    <CalculatorStateInLeaf/>
+                </div>
+
+                <button onClick={() => App.calculate()}>Calculate</button>
+            </div>
+        );
+    }
 }
 
 export default App;
