b7242ef53c01afe6b3db23a309a99a4482dae445
Thomas Sundberg <thomas@thinkcode.se>
2020-09-05 09:25:47 +0200

    Added an example with state in the leaves

--- moving-state-around/src/state-in-leaf/Minus.tsx
+++ moving-state-around/src/state-in-leaf/Minus.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import NumberField from "./NumberField";
 
 interface Props {
 }
@@ -7,9 +8,43 @@ interface State {
 }
 
 class Minus extends React.Component<Props, State> {
+    first: React.RefObject<NumberField>;
+    second: React.RefObject<NumberField>;
+
+    constructor(props: Props) {
+        super(props);
+
+        this.first = React.createRef();
+        this.second = React.createRef();
+    }
+
+    getResult(): number {
+        const first: number = this.getFirst();
+        const second: number = this.getSecond();
+
+        return first - second;
+    }
+
+    private getFirst() {
+        if (this.first.current !== null) {
+            return this.first.current.getValue();
+        }
+
+        return 0;
+    }
+
+    private getSecond() {
+        if (this.second.current !== null) {
+            return this.second.current.getValue();
+        }
+
+        return 0;
+    }
+
     render() {
         return <div>
-            Minus
+            <NumberField ref={this.first}/>
+            <NumberField ref={this.second}/>
         </div>
     }
 }
--- moving-state-around/src/state-in-leaf/NumberField.tsx
+++ moving-state-around/src/state-in-leaf/NumberField.tsx
@@ -4,12 +4,40 @@ interface Props {
 }
 
 interface State {
+    value: string
 }
 
 class NumberField extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            value: ''
+        }
+    }
+
+    getValue(): number {
+        return Number(this.state.value);
+    }
+
+    handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const dirty = e.currentTarget.value;
+        const number = Number(dirty);
+
+        if (!isNaN(number)) {
+            const value = '' + number;
+            this.setState({value: value});
+        }
+    }
+
     render() {
         return <div>
-            NumberField
+            <input value={this.state.value}
+                   onChange={(e) => {
+                       this.handleChange(e)
+                   }}
+            />
+
         </div>
     }
 }
