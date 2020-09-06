fda6fc8cd5de70657859419a2b590ac26dd3a5b5
Thomas Sundberg <thomas@thinkcode.se>
2020-09-06 10:53:53 +0200

    Added tests to cover the component

--- moving-state-around/src/state-in-leaves/NumberField.test.tsx
+++ moving-state-around/src/state-in-leaves/NumberField.test.tsx
@@ -3,6 +3,29 @@ import {shallow} from "enzyme";
 import NumberField from "./NumberField";
 
 test('should render without crashing', () => {
-    let wrapper = shallow(<NumberField/>);
+    shallow(<NumberField id={"non-numeric"}/>);
+});
+
+test('should connect the input to the on change handler', () => {
+    const event = {currentTarget: {value: "43"}};
+    let wrapper = shallow(<NumberField id={"non-numeric"}/>);
     const numberField = wrapper.instance() as NumberField;
+    const handleChange = jest.fn();
+    numberField.handleChange = handleChange;
+    const input = wrapper.find('#non-numeric');
+
+    input.simulate('change', event);
+
+    expect(handleChange).toBeCalledWith(event);
+});
+
+test('should prepare a value to be collected', () => {
+    const event: any = {currentTarget: {value: "43"}};
+    let wrapper = shallow(<NumberField id={"non-numeric"}/>);
+    const numberField = wrapper.instance() as NumberField;
+
+    numberField.handleChange(event);
+    const actual: number = numberField.getValue();
+
+    expect(actual).toEqual(43)
 });
